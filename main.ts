3f94207537263bdc7bdd7d804ae2a11214b5b8d3
cfalczuk <69084983+cfalczuk@users.noreply.github.com>
2022-01-05 18:41:02 -0300

    ahora puede multiplicar tocando el botón "a" y dividir tocando el botón "b"

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 let calculo = 0
 let calculo2 = 0
-basic.forever(function () {
+input.onButtonPressed(Button.A, function () {
     calculo = randint(0, 100)
     calculo2 = randint(0, 100)
     basic.showNumber(calculo)
@@ -8,5 +8,19 @@ basic.forever(function () {
     basic.showNumber(calculo2)
     basic.showString("=")
     basic.showNumber(calculo * calculo2)
+    basic.pause(500)
     basic.showString("_")
+    basic.pause(500)
+})
+input.onButtonPressed(Button.B, function () {
+    calculo = randint(0, 100)
+    calculo2 = randint(0, 100)
+    basic.showNumber(calculo)
+    basic.showString("\\")
+    basic.showNumber(calculo2)
+    basic.showString("=")
+    basic.showNumber(calculo + calculo2)
+    basic.pause(500)
+    basic.showString("_")
+    basic.pause(500)
 })
